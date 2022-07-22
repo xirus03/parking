2e0f7931b4a111769e33fcc66b0b93297186312a
Xirus03 <marklouiesoriano@gmail.com>
2022-07-22 17:12:05 +0800

    remove unnecessary comments

--- server.js
+++ server.js
@@ -14,20 +14,7 @@ const mongoose = require("mongoose");
   db.once("open", function () {
     console.log("Connected successfully");
   });
-
-  //   db.dropDatabase(function (err, result) {
-  //     console.log("deleted");
-  //   });
 })();
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 app.use(
   cors({
